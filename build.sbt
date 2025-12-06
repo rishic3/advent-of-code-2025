157ec4eb3de3d02de0cccd803b00ed666c066ffc
Rishi C. <rishic@nvidia.com>
2025-12-06 11:52:22 -0800

    initial commit

---
+++ build.sbt
@@ -0,0 +1,4 @@
+scalaVersion := "2.13.12"
+
+name := "advent-of-code-2025"
+version := "1.0"
---
+++ src/main/scala/aoc/Day1.scala
@@ -0,0 +1,7 @@
+package aoc
+
+object Day1 {
+    def main(args: Array[String]): Unit = {
+        println("hello")
+    }
+}
