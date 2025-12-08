0ae6c982cd225e8fdf447c0343a92010a93a83f6
Rishi C. <rishic@nvidia.com>
2025-12-07 16:57:21 -0800

    day 2

--- src/main/scala/aoc/Day2.scala
+++ src/main/scala/aoc/Day2.scala
@@ -7,9 +7,10 @@ import scala.math.BigInt
 object Day2 {
 
   // part 1
-  def isInvalid1(id: BigInt): Boolean = {
+  def isTwiceRepeated(id: BigInt): Boolean = {
     val idStr = id.toString
     val len = idStr.length()
+  
     if (len % 2 == 0) {
       val p1 = idStr.substring(0, len / 2)
       val p2 = idStr.substring(len / 2, len)
@@ -19,24 +20,38 @@ object Day2 {
     }
   }
 
+  // part 2
+  def isRepeated(id: BigInt): Boolean = {
+    val s = id.toString
+    val len = s.length()
+
+    val candidateSizes = (1 to len / 2).view.filter(sz => len % sz == 0)
+    candidateSizes.exists { sz =>
+      val iter = s.grouped(sz)
+      val substr = iter.next()
+      iter.forall(_ == substr)
+    }
+  }
+
   def parseLine(line: String): Array[(BigInt, BigInt)] = {
     line.trim().split(',').map(_.split('-')).map(arr => (BigInt(arr(0)), BigInt(arr(1))))
   }
 
   def computeInvalidIDSum(line: String): BigInt = {
     val ranges = parseLine(line)
-    ranges.foldLeft(BigInt(0)) { case (acc, (low, high)) =>
-      val range = low to high
-      acc + range.foldLeft(BigInt(0)) { case (acc, id) =>
-        if (isInvalid1(id)) acc + id else acc
-      }
-    }
+    ranges.map { case (low, high) =>
+      (low to high).view.filter(isRepeated).sum
+    }.sum
   }
 
   def main(args: Array[String]): Unit = {
     assert(args.length > 0, s"please provide input path")
 
-    val source = Source.fromFile(args(0)).mkString
-    println(s"Sum of invalid IDs: ${computeInvalidIDSum(source)}")
+    val source = Source.fromFile(args(0))
+    try {
+      println(s"Sum of invalid IDs: ${computeInvalidIDSum(source.mkString)}")
+    } finally {
+      source.close()
+    }
   }
 }
